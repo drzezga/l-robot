c8859545dfeecd6ae6bd9e1954e669775ffc525c
drzezga <19654689+drzezga@users.noreply.github.com>
2021-06-11 16:07:23 +0200

    More work on the resolver

--- src/resolver.rs
+++ src/resolver.rs
@@ -10,19 +10,40 @@ pub enum ResolveMessageType {
     Output,
 }
 
-pub enum ResolveValue {
+pub enum NamespaceElement {
     Number(f64),
-    // BigNum(f64), // TODO: More types
-    // Matrix(f64)
+    BigNum(f64), // TODO: More types
+    Matrix(f64)
 }
+
+type Namespace<'a> = HashMap<&'a str, f64>;
 
 pub fn resolve(nodes: Vec<(usize, ASTNode)>) -> Vec<(ResolveMessageType, String)> {
     let out = Vec::new();
-    let variables: HashMap<&str, ResolveValue> = HashMap::new();
+    let namespace: Namespace = HashMap::new();
 
-    for (line_num, node) in nodes {
-        post_order(node, )
+    for (line_num, mut node) in nodes {
+        // let mut reference = &mut node;
+        post_order(&mut node, &|x| {
+            match &mut x.node_type {
+                crate::parser::ASTNodeType::Sum => todo!(),
+                crate::parser::ASTNodeType::Difference => todo!(),
+                crate::parser::ASTNodeType::Product => todo!(),
+                crate::parser::ASTNodeType::Quotient => todo!(),
+                crate::parser::ASTNodeType::Power => todo!(),
+                crate::parser::ASTNodeType::Equality => todo!(),
+                crate::parser::ASTNodeType::Function(f_name) => {
+
+                },
+                crate::parser::ASTNodeType::Empty => (), // throw error
+                crate::parser::ASTNodeType::Delimeter(_) => (),
+            }
+        });
     }
 
     out
+}
+
+pub fn resolve_fn(namespace: &Namespace) -> Result<ASTNode, (ResolveMessageType, String)> {
+    todo!()
 }
